acd1073e182427883a6fe1b8c8fa58d1901183d8
tom <tom.dobs@fltdpl.de>
2015-09-07 23:33:27 +0900

    removed all httpauth components

--- app.py
+++ app.py
@@ -3,31 +3,13 @@
 
 from flask import Flask, render_template, jsonify, request
 from flask import abort, make_response
-from flask.ext.httpauth import HTTPBasicAuth
 import highscore
 
 
 app = Flask(__name__)
 app.config.update(
   DEBUG=True,
-  SECRET_KEY='ub1jvg94jd9ghz490'
 )
-
-
-
-auth = HTTPBasicAuth()
-
-
-@auth.get_password
-def get_password(username):
-    if username == 'jie993nkvlae83n3n275bvuxy':
-        return 'xvu22cxytsaaslf8302rvie99'
-    return None
-
-
-@auth.error_handler
-def unauthorized():
-    return make_response(jsonify({'error': 'Unauthorized access'}), 403)
 
 
 @app.errorhandler(404)
@@ -42,7 +24,6 @@ def index():
 
 
 @app.route('/buddha/api/highscore', methods=['GET'])
-@auth.login_required
 def get_scores():
     scores = highscore.printHighscore()
     if len(scores) == 0:
@@ -52,7 +33,6 @@ def get_scores():
 
 
 @app.route('/buddha/api/highscore', methods=['POST'])
-@auth.login_required
 def create_score():
     if not request.json or not 'name' in request.json:
         abort(400)
